0d9fe0a559518c55694e4a37aacce73ae7782165
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-16 01:48:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,6 +64,7 @@ class Owner
     fish_count = @pets[:fishes].count
     dog_count = @pets[:dogs].count
     cat_count = @pets[:cats].count
+    "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
   end
 # I have 2 fish, 3 dog(s), and 1 cat(s).
 end
