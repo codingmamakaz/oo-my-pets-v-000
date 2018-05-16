febcc6dade931893793ebd0d6a1a57bdc6620114
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-16 01:44:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,7 @@ class Owner
 
   def list_pets
     binding.pry
-    @pets[:fishes].count {|fish| "I have #{fish} fish"}
+    @pets[:fishes].count 
   end
 # I have 2 fish, 3 dog(s), and 1 cat(s).
 end
